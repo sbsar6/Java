d4bad372ca4319e16dfd1973fbea8f2532473565
sbsar6 <ajr.hpc@gmail.com>
2015-03-30 12:26:43 +0100

    Revert "Updated eysys and lists"
    This reverts commit b5efcb25aaf436749355694c7edc796cda0de67e.

--- NetBeansProjects/Lists/src/lists/Lists.java
+++ NetBeansProjects/Lists/src/lists/Lists.java
@@ -4,8 +4,7 @@
  * and open the template in the editor.
  */
 package lists;
-import java.io.*;
-
+import java.util.ArrayList;
 /**
  *
  * @author Andrew
@@ -16,68 +15,15 @@ public class Lists {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    double seScore = 0;
-    double accounScore = 0;
-    double quantScore = 0;
-    double archScore = 0;
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("Alpha");
+    list.add("Delta");
+    list.add("Charlie");
+        System.out.println("List: " + list);
     
-        System.out.println("Please enter title to check for normalisation");
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader buffer = new BufferedReader(isr);
-        String normal = "";
-        
-        try
-        { 
-            normal = buffer.readLine();
-            buffer.close();          
-        }
-        catch(IOException e) 
-        {
-        
-            System.out.println("An input error has occured");
-                }
-        
-    if (normal.contains("Engineer")||normal.contains("engineer")&& normal.contains("Software")||normal.contains("software")){
-        seScore =seScore +1;
+        System.out.println("Replacing " + list.get(1) + "\n");
+        list.set(1, "Bravo");
+    list.forEach((x) -> System.out.println("Item: " + x));
     }
     
-    else if (normal.contains("Engineer")||normal.contains("engineer")|| normal.contains("Software")||normal.contains("software")){
-        quantScore =quantScore +0.5;}
-    
-    else if (normal.contains("Quantity")||normal.contains("quantity")&& normal.contains("Surveyor")||normal.contains("surveyor")){
-        quantScore =quantScore +1;
-    }
-    
-    else if (normal.contains("Quantity")||normal.contains("quantity")|| normal.contains("Surveyor")||normal.contains("surveyor")){
-        quantScore =quantScore +0.5;}
-    
-    if (normal.contains("Architect")||normal.contains("architect")){
-        archScore =archScore +1;
-    }
-    if (normal.contains("Accountant")||normal.contains("accountant")){
-        accounScore =accounScore +1;
-    }
-    else {
-        System.out.println("No match found");
-        return;
-    }
-    
-    if (seScore > quantScore && seScore > archScore && seScore > accounScore){
-        System.out.println("Normalisation is Software Engineer");
-    }
-    else if (quantScore > seScore && quantScore> archScore && quantScore > accounScore){
-        System.out.println("Normalisation is Quantity Surveyor");
-    }
-    else if (archScore > seScore && archScore> quantScore && archScore> accounScore){
-        System.out.println("Normalisation is Architect");
-    
-    }
-    else if (accounScore > seScore && accounScore> quantScore && accounScore> archScore){
-        System.out.println("Normalisation is Accountant");
-    
-    }
-    else {
-        System.out.println("No normalisation found, has similarities with more than one type");
-    }
-}
 }
