191dadc307ca01d6a108c459852370a6bea6b0bb
sbsar6 <ajr.hpc@gmail.com>
2015-03-31 16:26:44 +0100

    extra bits

--- NetBeansProjects/Eysys2/src/eysys2/Eysys2.java
+++ NetBeansProjects/Eysys2/src/eysys2/Eysys2.java
@@ -23,42 +23,40 @@ public class Eysys2 {
           
 } 
    //creating getStats method to run on input arrays
-      public static int getStats(int[]a){
-        HashMap stats = new HashMap();  
-   stats.put("Mean value is ", mean(a));
-   stats.put("Median value is ", median(a));
-   stats.put("Modal value is ", mode(a));
-   stats.put("Range value is " , range(a));
-   
-   System.out.println(stats.toString()); 
-        return 0;
+    public static int getStats(int[]a){
+    HashMap stats = new HashMap();  
+    stats.put("Mean value is ", mean(a));
+    stats.put("Median value is ", median(a));
+    stats.put("Modal value is ", mode(a));
+    stats.put("Range value is " , range(a));
+    System.out.println(stats.toString()); 
+    return 0;
       }
       
      //Calculating mean from array  
-      public static int mean(int[] a) {
- 
-     int meanSum = 0;
+    public static int mean(int[] a) {
+    int meanSum = 0;
     for (int i = 0; i < a.length; i++) {
         meanSum += a[i];
     }
     return meanSum / a.length;
-}
+    }
     
     //Calculating median from array
     public static int median(int[] a) {
     int midSum = a.length/2;
     if (a.length%2 == 1) {
         return a[midSum];
-    } else {
+    } 
+    else {
         return (int) ((a[midSum-1] + a[midSum]) / 2.0);
     }
-}
+    }
     
     //Calculating mode from array
     public static int mode(int a[]) {
     int maxValue = 0;
     int maxCount = 0;
-
     for (int i = 0; i < a.length; ++i) {
         int count = 0;
         for (int j = 0; j < a.length; ++j) {
@@ -71,22 +69,21 @@ public class Eysys2 {
     }
     return maxValue;
 
-}
+    }
     
     public static int range(int a[]){
-       int max = a[0];
-        int min = a[0];
-        for (final int i : a) {
-            if (i > max) {
-                max = i;
-            } else if (i < min) {
+    int max = a[0];
+    int min = a[0];
+    for (final int i : a) {
+        if (i > max) {
+            max = i;
+        } 
+        else if (i < min) {
                 min = i;
-            }
         }
-        //return min;
-        //return max;
-        return (max - min) + 1;
-        
+    }
+    //Get full range
+    return (max - min) + 1;   
     }
     
 }
